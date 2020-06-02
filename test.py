6875fa753d631ad4b572e221041a3a0f1060f2b2
maciek666 <czerwoneszelki19@gmail.com>
2020-06-02 11:52:51 +0200

    kombinacje

--- test.py
+++ test.py
@@ -15,100 +15,112 @@ def power(x, m, n):
     return a
 
 
+def bytes_per_pixel(color_type):
+    switcher = {
+        0: 1,
+        2: 3,
+        3: 1,
+        4: 2,
+        6: 4,
+    }
+    return switcher.get(color_type, "Not found")
+
+
 def int_to_bytes(x: int) -> bytes:
     return x.to_bytes((x.bit_length() + 7) // 8, 'big')
 
 
-def decrypt(private_key, N, encrypted_data, key_size,leng):
+def decrypt(private_key, N, encrypted_data, key_size, pixel):
     msg = b''
     blocks = []
-    # print(f'k/8 = {key_size // 8}')
     d = None
+    rest = len(encrypted_data) % pixel
     for d in encrypted_data:
         x = d[0]
+        # print(d)
         if d[1] is not None:
             x += d[1]
         blockInt = int(x, 16)
         decodedInt = power(blockInt, private_key, N)
-        # decodedHex = format(decodedInt, 'x')
         decodedHex = int_to_bytes(decodedInt)
         blocks.append(decodedInt)
         # print(len(x))
-        if d != encrypted_data[-1]:
-
-            while len(decodedHex) < key_size / 8:
-                decodedHex = b'\x00' + decodedHex
-
+        if d is not encrypted_data[-1]:
+            while len(decodedHex) < pixel // 2:
+                decodedHex = b'\0' + decodedHex
         else:
-            while leng - len(msg+decodedHex) > 0:
-                decodedHex = b'\x00' + decodedHex
-            print(int_to_bytes(blockInt))
+            while len(decodedHex) < pixel // 2:
+                decodedHex = b'\0' + decodedHex
+
+            # print(int_to_bytes(blockInt))
         # print(decodedHex)
         msg += decodedHex
-    print(f'last d = {len(d)}')
-    print(f'dec = {blocks[5000:5200]}')
+    # print(f'dec = {blocks[5000:5200]}')
     return msg, blocks
 
 
-def encrpyt(public_key, N, data, key_size):
+def encrpyt(public_key, N, data, pixel):
     input_size = len(data)
-    block_size = key_size // 4
+    block_size = pixel
     print(public_key.bit_length())
     output = []
-    last = (input_size % block_size) / 2
-    print(f'last = {last}')
+
     ints = []
     block = None
-    print(data[0:20])
     for i in range(0, len(data), block_size):
         block = data[i:block_size + i]
-
+        # print(f'block = {block}')
         blockInt = int(block, 16)
         ints.append(blockInt)
 
         codedInt = int(power(blockInt, public_key, N))
-        codedHex = []
-        codedHex.append(format(codedInt, 'x'))
-        # codedHex.append(codedInt.to_bytes(((codedInt.bit_length() + 7) // 8),"big").hex())
+        codedHex = format(codedInt, 'x')
+        value_rest = []
+        while len(codedHex) < len(block):
+            codedHex = '0' + codedHex
 
-        # print( )
-        if len(codedHex) > len(block):
-            print(f'coded is longer {len(codedHex) - len(block)}')
-            codedHex = [codedHex[:len(block) + 1], codedHex[len(block) + 1:]]
+        if len(codedHex) > block_size:
+            # print(f'coded is longer {len(codedHex[0]) - len(block)}')
+            value_rest = [codedHex[:len(block)], codedHex[len(block):]]
+
+            # print(f'codedHex= {codedHex}')
         else:
-            codedHex.append(None)
-        while len(codedHex[0]) < len(block):
-            codedHex[0] = '0' + codedHex[0]
+            value_rest.append(codedHex)
+            value_rest.append(None)
 
-        output.append(codedHex)
+        output.append(value_rest)
 
-    # print(block)
-    print(f'enc = {ints[5000:5200]}')
+    # print(f'enc = {ints[5000:5200]}')
     return output, ints
 
 
-cls = image.image('kostki.png')
-# data = cls.idat
-data_xx = zlib.decompress(cls.idat)
+cls = image.image('papuga_anon.png')
+pixel = bytes_per_pixel(cls.colour_type)
+print(f'color:= {pixel}')
+
+idat = binascii.unhexlify(cls.idat)
+data_xx = zlib.decompress(idat)
 data = image.ToHex_str(data_xx)
 
 rsa = RSA()
-key_size = 128
+key_size = 4
 public_key, privaye_key, N = rsa.key_generator(key_size)
-output, ints_before_enc = encrpyt(public_key, N, data, key_size)
+output, ints_before_enc = encrpyt(public_key, N, data, pixel)
 # print(output[:20])
+sum = ''
+for o in output:
+    sum += o[0]
+sum = binascii.unhexlify(sum)
+sum = zlib.compress(sum, 1)
+# cls.saveImageWithIDAT('rsa.png', sum)
+dec, ints_after_dec = decrypt(privaye_key, N, output, key_size, pixel)
+print('dlugosci danych ', len(sum), len(idat))
 
-dec, ints_after_dec = decrypt(privaye_key, N, output, key_size,len(data_xx))
-
-# ints_after_dec = list(map(lambda x: b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' if x == b'' else x ,ints_after_dec ))
-# n = list(filter(lambda x: x != 0, data_xx))
-# print(data_xx[:20])
-# print(dec[0:20])
-# dec = dec[:len(dec)]
+print(output[:20])
 print(ints_after_dec == ints_before_enc)
 print(len(dec), len(data_xx))
-print(dec == data_xx)
+print(dec[:len(data_xx)] == data_xx)
 
 x = 25000
-#print(dec[:-5])
-#print(data_xx[:-5])
+# print(dec[:-5])
+# print(data_xx[:-5])
